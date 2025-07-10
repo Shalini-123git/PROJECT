bb5d25a5875f263bdf49485a0494d06ad9bc67e5
shalini rajput <rajputshalini8744@gmail.com>
2025-07-11 01:36:51 +0530

    update

--- routes/register.js
+++ routes/register.js
@@ -13,18 +13,25 @@ router.get("/", (req, res) => {
 
 //create route
 router.post("/post", upload.single("image"), async (req, res) => {
-    let url = req.file.path;
-    let filename = req.file.filename;
+    try {
+        if(!req.file){
+            return res.status(400).send("No file uploaded");
+        }
+    const {path: url, filename } = req.file;
     let data = req.body;
     const newReport = new Report({url, filename});
     const newPatient = new Patient(data);
-    newPatient.image.save = {url, filename};
+    newPatient.image = {url, filename};
 
     await newPatient.save();
     await newReport.save();
 
     
     res.render("show.ejs", {url, filename});
-})
+    } catch {
+        console.error("Error while saving data", err);
+        res.status(500).send("Internal Seerver Error.");
+    }
+});
 
 module.exports = router;
